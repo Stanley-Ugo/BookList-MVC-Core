a28fa2ba20788b17d5d3c424b1d33f9bf403a09b
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-24 00:26:08 +0100

    Adding a Book Dbset

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -8,6 +8,10 @@ namespace BookList.Models
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {}
 
+        public DbSet<Book> Books { get; set; }
     }
 }
