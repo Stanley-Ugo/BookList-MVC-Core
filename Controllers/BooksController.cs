6c2c4fd088c90d84af590031030378ae3baa191c
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-24 01:18:24 +0100

    Injecting ApplicationDbContext into Books Controller & Disposing it after use

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookList.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookList.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
         }
     }
 }
