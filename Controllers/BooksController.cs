b028743468e4695e37d3a1d3ff5898b3c2e00078
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-24 01:49:10 +0100

    Adding Books - index IActionResult View

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -17,7 +17,7 @@ namespace BookList.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Books.ToList());
         }
 
         protected override void Dispose(bool disposing)
