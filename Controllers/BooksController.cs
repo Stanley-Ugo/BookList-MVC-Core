549c926824f539b1a3cecf1d57e8873592697e8d
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-25 13:39:34 +0100

    Adding a Details action to Books Controller

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookList.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookList.Controllers
 {
@@ -36,8 +37,23 @@ namespace BookList.Controllers
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(book);
 
+            return View(book);
+        }
+
+        //Details : Books/Details/:id
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
         }
 
         protected override void Dispose(bool disposing)
